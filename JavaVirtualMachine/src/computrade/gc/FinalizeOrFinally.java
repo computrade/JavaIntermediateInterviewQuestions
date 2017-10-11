6316f13e65c2a8c968340e3d2f169c9303aa0b18
Yuval Ishay <yuval@computrade.co.il>
2017-10-11 20:55:08 +0300

    FinalizeOrFinally first version

---
+++ JavaVirtualMachine/src/computrade/gc/FinalizeOrFinally.java
@@ -0,0 +1,50 @@
+package computrade.gc;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class FinalizeOrFinally {
+
+	PrintWriter out = null;
+
+	public static void main(String... args) throws InterruptedException {
+
+		FinalizeOrFinally finalizeOrFinally = new FinalizeOrFinally();
+		finalizeOrFinally.writeFile();
+
+		finalizeOrFinally = null;
+
+		System.gc();
+		Thread.sleep(1000);
+
+	}
+
+	public void writeFile() {
+
+		try {
+			System.out.println("Entering" + " try statement");
+			out = new PrintWriter(new FileWriter("OutFile.txt"));
+
+		} catch (IOException e) {
+
+			System.err.println("Caught IOException: " + e.getMessage());
+
+		} finally {
+			if (out != null) {
+				System.out.println("finally block: Closing PrintWriter");
+				out.close();
+			} else {
+				System.out.println("finally block: PrintWriter not open");
+			}
+		}
+	}
+
+	@Override
+	public void finalize() {
+
+		// TODO implement your own finalize block.
+	
+	}
+
+}
