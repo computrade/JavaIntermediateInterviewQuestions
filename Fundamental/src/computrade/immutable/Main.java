38b6527f09d00337439668ffaf4e5d90fe2fc854
Yuval Ishay <yuval@computrade.co.il>
2017-11-03 07:54:31 +0200

    comment

--- Fundamental/src/computrade/immutable/Main.java
+++ Fundamental/src/computrade/immutable/Main.java
@@ -11,15 +11,13 @@ public class Main {
 	public void run(){
 		
 		Address immutableAdress = new Address(16, "Pine st." , "Los Angeles", "California");
-		Student immutableStudent = new Student(123,"Clinton",immutableAdress);
+		Student immutableStudent = new Student(1,"Bob",immutableAdress);
 		
 		System.out.println("This is my immutable student:\n" + immutableStudent);
 		
-		Address adress = immutableStudent.getAdress();
-		adress.setStreet("Apple st.");
-		
-		System.out.println("This is my immutable student ?:\n" + immutableStudent);
-		
+		//Address adress = immutableStudent.getAdress();
+		//adress.setStreet("Apple st.");
+		//System.out.println("This is my immutable student ?:\n" + immutableStudent);
 		
 	}
 
