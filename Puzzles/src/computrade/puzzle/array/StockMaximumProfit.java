f57a8761c8245bf5f5c191255a4813e94bc4ba8b
Yuval Ishay <yuval@computrade.co.il>
2017-11-24 08:22:31 +0200

    update stock max profit

--- Puzzles/src/computrade/puzzle/array/StockMaximumProfit.java
+++ Puzzles/src/computrade/puzzle/array/StockMaximumProfit.java
@@ -34,14 +34,18 @@ package computrade.puzzle.array;
  */
 public class StockMaximumProfit {
 
-	public static double findBestProfit(double stockPricesYesterday[]) {
+	public static double findBestProfit(double stockPrices[]) {
+		
+		if (stockPrices.length < 2) {
+		        throw new IllegalArgumentException("Getting a profit requires at least 2 prices.");
+		}
 
-		double min_price = stockPricesYesterday[0];
+		double min_price = stockPrices[0];
 		double max_profit = 0.0;
 
-		for (int i = 0; i < stockPricesYesterday.length; i++) {
+		for (int i = 0; i < stockPrices.length; i++) {
 
-			double current_price = stockPricesYesterday[i];
+			double current_price = stockPrices[i];
 			min_price = Math.min(min_price, current_price);
 			max_profit = Math.max(max_profit, current_price - min_price);
 
--- Puzzles/src/computrade/puzzle/array/StockMaximumProfitTest.java
+++ Puzzles/src/computrade/puzzle/array/StockMaximumProfitTest.java
@@ -1,6 +1,6 @@
 package computrade.puzzle.array;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 import org.junit.Test;
 
@@ -8,45 +8,56 @@ import org.junit.Test;
 public class StockMaximumProfitTest {
 	
 	@Test
+	public void testStockMaximumProfitException() {
+		
+		try {
+			double stockPrices[] = { 100 };
+			StockMaximumProfit.findBestProfit(stockPrices);
+		    fail( "My method didn't throw when I expected it to" );
+		} catch (IllegalArgumentException expectedException) {
+		}
+	}
+	
+	@Test
 	public void testStockMaximumProfit2num() {
 		
-		double stockPricesYesterday[] = { 100, 100.2 };	
-		assertEquals(0.2,StockMaximumProfit.findBestProfit(stockPricesYesterday),0.001);
+		double stockPrices[] = { 100, 100.2 };	
+		assertEquals(0.2,StockMaximumProfit.findBestProfit(stockPrices),0.001);
 	
 	}
 	
 	@Test
 	public void testStockMaximumProfit3num() {
 		
-		double stockPricesYesterday[] = { 100, 100.2, 101 };
-		assertEquals(1,StockMaximumProfit.findBestProfit(stockPricesYesterday),0.001);
+		double stockPrices[] = { 100, 100.2, 101 };
+		assertEquals(1,StockMaximumProfit.findBestProfit(stockPrices),0.001);
 	
 	}
 	
 	@Test
 	public void testStockMaximumProfit4num() {
 		
-		double stockPricesYesterday[] = { 100, 100.2, 99, 98 };
-		assertEquals(0.2,StockMaximumProfit.findBestProfit(stockPricesYesterday),0.001);
+		double stockPrices[] = { 100, 100.2, 99, 98 };
+		assertEquals(0.2,StockMaximumProfit.findBestProfit(stockPrices),0.001);
 	
 	}
 	
 	@Test
 	public void testStockMaximumProfit5Num() {
 		
-		double stockPricesYesterday[] = { 100, 100.2, 99, 98, 103 };
-		assertEquals(5,StockMaximumProfit.findBestProfit(stockPricesYesterday),0.001);
+		double stockPrices[] = { 100, 100.2, 99, 98, 103 };
+		assertEquals(5,StockMaximumProfit.findBestProfit(stockPrices),0.001);
 	
 	}
 
 	@Test
 	public void testStockMaximumProfitRandomNum() {
 		
-		double stockPricesYesterday[] = { 100, 100.2, 99, 98, 97.1, 99, 96,
+		double stockPrices[] = { 100, 100.2, 99, 98, 97.1, 99, 96,
 				101.2, 102.3, 104.1, 103.2 };
 		
 		
-		assertEquals(8.1,StockMaximumProfit.findBestProfit(stockPricesYesterday),0.001);
+		assertEquals(8.1,StockMaximumProfit.findBestProfit(stockPrices),0.001);
 	
 	}
 	
@@ -54,8 +65,8 @@ public class StockMaximumProfitTest {
 	@Test
 	public void testStockMaximumProfitInteger() {
 		
-		double stockPricesYesterday[] = { 100, 99, 101, 98, 102 };
-		assertEquals(4,StockMaximumProfit.findBestProfit(stockPricesYesterday),0.001);
+		double stockPrices[] = { 100, 99, 101, 98, 102 };
+		assertEquals(4,StockMaximumProfit.findBestProfit(stockPrices),0.001);
 	
 	}
 	
