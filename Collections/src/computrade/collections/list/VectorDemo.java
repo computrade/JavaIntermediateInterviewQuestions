e3aa97679e96810e01cfb481bbc6cb103be615cd
Yuval Ishay <yuval@computrade.co.il>
2018-01-01 19:18:33 +0200

    update Vector code.

--- Collections/src/computrade/collections/list/VectorDemo.java
+++ Collections/src/computrade/collections/list/VectorDemo.java
@@ -10,17 +10,16 @@ public class VectorDemo {
 	  Vector<Student> students = new Vector<Student>();
 
 	  // Create a few Student objects.
-	  Student a = new Student("Herbie");
-	  Student b = new Student("Clem");
-	  Student c = new Student("Oscar");
+	  Student a = new Student("Alice");
+	  Student b = new Student("Bob");
+	  Student c = new Student("Charly");
 
 	  // Store references to all three Students in the collection.
 	  students.add(a);
 	  students.add(b);
 	  students.add(c);
 
-	  // ... and then iterate through them one by one,
-        // printing each student’s name.
+	  // iterate through and print them one by one,
 	  for (Student s : students) {
 	    System.out.println(s.getName()); 
 	  }
